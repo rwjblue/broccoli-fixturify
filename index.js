9e4b18a91a8d11fbf749c6071d6a1905589af400
Stefan Penner <stefan.penner@gmail.com>
2016-07-31 17:16:07 -0700

    Make broccoli-fixturify suitable for rebuild testing
    * switch to stable “persistent” output
    * expose Fixturify.prototype.write(obj) Fixturify.prototype.remove(obj) to allow test consumers to mutate the node this plugin maintains
    * add Fixturify.prototype.reset() which resets the fixtures to what they would have been on the first build. This is accomplished by removing the outputDirectory, recreating it, and apply the original fixtures.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Plugin = require('broccoli-plugin');
 var fixturify = require('fixturify');
+var fs = require('fs-extra');
 
 module.exports = Fixturify;
 Fixturify.prototype = Object.create(Plugin.prototype);
@@ -11,10 +12,37 @@ function Fixturify(fixture, options) {
     annotation: options.annotation
   });
 
-  this.fixture = fixture;
+  this._persistentOutput = true;
+  this._initialFixture = this._fixture = fixture;
+  this._remove = {};
+  this._purgeOnNextBuild = false;
 }
 
+Fixturify.prototype.write  = function(fixture) {
+  this._fixture = require('lodash.merge')(this.fixture, fixture);
+};
+
+Fixturify.prototype.remove = function(fixture) {
+  this._remove = require('lodash.merge')(this._remove, fixture);
+};
+
+Fixturify.prototype.reset = function() {
+  this._fixture = this._initialFixture;
+  this._purgeOnNextBuild = true;
+};
+
 Fixturify.prototype.build = function() {
-  fixturify.writeSync(this.outputPath, this.fixture);
+  if (this._purgeOnNextBuild) {
+    this._purgeOnNextBuild = false;
+    fs.removeSync(this.outputPath);
+    fs.mkdirSync(this.outputPath);
+  }
+  try {
+    fixturify.removeSync(this.outputPath, this._remove);
+    fixturify.writeSync(this.outputPath,  this._fixture);
+  } finally {
+    this._fixture = {};
+    this._remove  = {};
+  }
 };
 
